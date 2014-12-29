34e14312158d712d4dc63296d2ca901b39b7ae3a
Colin Fuller <colin@khanacademy.org>
2014-12-29 11:18:35 -0500

    Change do syntax

--- lights-main.rkt
+++ lights-main.rkt
@@ -6,8 +6,8 @@
 (init)
 
 (define (cycle)
-  (today-at 8 0 do (on))
-  (today-at 20 0 do (off))
-  (tomorrow-at 0 1 do (cycle)))
+  (today-at 8 0 (on))
+  (today-at 20 0 (off))
+  (tomorrow-at 0 1 (cycle)))
 
 (cycle)
